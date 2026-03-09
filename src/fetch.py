9d28aa252a2244389ac19aac9399b3d8680dc8a0
Gustav <gustavbjorelius@gmail.com>
2026-03-09 19:51:11 +0100

    wip kalshi fetsher skeleton

---
+++ src/fetch.py
@@ -0,0 +1,31 @@
+#!/bin/python 
+
+import request
+
+def fetch_markets():
+    """ f that fetches market live-data """
+
+    url = "https://api-election.kalshi.com/markets"
+    r = request.get(url)
+    data = r.json()
+     
+    there was something else here
+    and here? 
+
+def print_markets():
+    """ bla bla bla good documentation"""
+
+for i in enumerate(data):
+    then something here that would take out the key which was 
+    and here there were another thing
+    and a last(?) thing here
+
+
+
+if __name__ = '__main__':
+    print(markets)
+    print()
+
+
+
+
